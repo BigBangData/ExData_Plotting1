1e2862c63793b960035dba7e55b742f2a3adb24c
Marcelo Sanches <30713252+BigBangData@users.noreply.github.com>
2017-11-18 10:23:39 -0700

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,7 +1,8 @@
+## provided the unzipped data for this project is in your working directory (directions to download data in README)
+
 ## reads in subsetted data, adds DateTime variable
 hpc <- subset(read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?"), Date == "1/2/2007" | Date == "2/2/2007")
 hpc$DateTime <- strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S")
-hpc <- hpc[, 3:10]
 
 ## creates plot 1 
 hist(hpc[, 1], main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red") 
