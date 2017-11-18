9e50578dca5b4340d360d17785b306162624218e
Marcelo Sanches <30713252+BigBangData@users.noreply.github.com>
2017-11-18 10:16:45 -0700

    Add files via upload

---
+++ plot1.r
@@ -0,0 +1,9 @@
+## reads in subsetted data, adds DateTime variable
+hpc <- subset(read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?"), Date == "1/2/2007" | Date == "2/2/2007")
+hpc$DateTime <- strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S")
+hpc <- hpc[, 3:10]
+
+## creates plot 1 
+hist(hpc[, 1], main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red") 
+dev.copy(png, file = "plot1.png", width = 480, height = 480) 
+dev.off()
---
+++ plot3.r
@@ -0,0 +1,13 @@
+## reads in subsetted data, adds DateTime variable
+hpc <- subset(read.table("./exdata_data_household_power_consumption/household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?"), Date == "1/2/2007" | Date == "2/2/2007")
+hpc$DateTime <- strptime(paste(hpc$Date, hpc$Time), "%d/%m/%Y %H:%M:%S")
+hpc <- hpc[, 3:10]
+
+## creates plot 3
+plot(hpc[, 8], hpc[, 5], ylab =  "Energy sub metering", xlab = "", type = "n")
+lines(hpc[, 8], hpc[, 5])
+lines(hpc[, 8], hpc[, 6], col="red")
+lines(hpc[, 8], hpc[, 7], col="blue")
+legend("topright",  lty = 1 , col = c("black", "red", "blue"), legend = c(names(hpc[5:7])))
+dev.copy(png, file = "plot3.png", width = 480, height = 480) 
+dev.off()
